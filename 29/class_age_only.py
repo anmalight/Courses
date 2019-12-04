3be7ae961e2f48f4798b966b1f9dfa73ef9a865b
anmalight <nastiamisiura@gmail.com>
2019-12-04 21:49:10 +0200

    h_w_29

--- 29/class_age_only.py
+++ 29/class_age_only.py
@@ -1,4 +1,3 @@
-# !!!!
 import datetime
 
 
@@ -6,32 +5,25 @@ class HumanAge:
 
     def __init__(self, b_day):
         self.b_day = b_day
-        self.valid_b_day = self.validate(b_day)
-        self.date_of_birth = self.str_to_datetime()
         self.date_now = datetime.datetime.now(tz=None)
+        self.date_of_birth = self.validate_n_convert(b_day, self.date_now)
 
     @staticmethod
-    def validate(b_day):
+    def validate_n_convert(b_day, date_now):
         try:
-            datetime.datetime.strptime(b_day, '%d-%m-%Y')
-            return b_day
+            date_of_birth = datetime.datetime.strptime(b_day, '%d-%m-%Y')
+            if date_of_birth <= date_now:
+                return date_of_birth
         except Exception as e:
             print(e)
 
-    def str_to_datetime(self):
-        date_of_birth = datetime.datetime.strptime(self.b_day, '%d-%m-%Y')
-        return date_of_birth
-
     @property
     def age(self):
-        if self.date_of_birth <= self.date_now:
-            age = self.date_now.year - self.date_of_birth.year - \
-                  ((self.date_now.month, self.date_now.day) < (self.date_of_birth.month, self.date_of_birth.day))
-            return f"{age} year(s)"
-        else:
-            raise Exception("You have not yet been born")
+        age = self.date_now.year - self.date_of_birth.year - \
+              ((self.date_now.month, self.date_now.day) < (self.date_of_birth.month, self.date_of_birth.day))
+        return f"{age} year(s)"
 
 
 human = HumanAge('07-12-1999')
-# human = HumanAge('07-12-2055')
+# human = HumanAge('07-12-2020')
 print(human.age)
