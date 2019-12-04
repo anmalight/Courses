f289c429e9924066bcc12c12070ae0931b18dce6
anmalight <nastiamisiura@gmail.com>
2019-12-04 22:35:12 +0200

    h_w_29

--- 29/class_age_only.py
+++ 29/class_age_only.py
@@ -1,10 +1,8 @@
 import datetime
 
-
 class HumanAge:
 
     def __init__(self, b_day):
-        self.b_day = b_day
         self.date_now = datetime.datetime.now(tz=None)
         self.date_of_birth = self.validate_n_convert(b_day, self.date_now)
 
@@ -12,7 +10,9 @@ class HumanAge:
     def validate_n_convert(b_day, date_now):
         try:
             date_of_birth = datetime.datetime.strptime(b_day, '%d-%m-%Y')
-            if date_of_birth <= date_now:
+            if date_of_birth >= date_now:
+                raise Exception('Not relevant date!')
+            else:
                 return date_of_birth
         except Exception as e:
             print(e)
@@ -23,7 +23,7 @@ class HumanAge:
               ((self.date_now.month, self.date_now.day) < (self.date_of_birth.month, self.date_of_birth.day))
         return f"{age} year(s)"
 
-
+# intercalary year
 human = HumanAge('07-12-1999')
 # human = HumanAge('07-12-2020')
 print(human.age)
